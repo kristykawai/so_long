740d57bfaf24772b0d8bf862d140771ef4895714
kchan <kristy.kw.chan@gmail.com>
2023-11-14 14:50:55 +1030

    initialization

--- src/main.c
+++ src/main.c
@@ -6,16 +6,27 @@
 /*   By: kchan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:46:28 by kchan             #+#    #+#             */
-/*   Updated: 2023/11/14 13:04:52 by kchan            ###   ########.fr       */
+/*   Updated: 2023/11/14 14:49:38 by kchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-#include <stdio.h>
-int	main(int argc, char	**argv)
+
+// int	main(int argc, char	**argv)
+// {
+// 	write(1,"y",1);
+// 	printf("sth:%d", argc);
+// 	printf("sth:%s", argv[1]);
+// 	return (0);
+// }
+
+//tutorial https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html#introduction
+int	main(void)
 {
-	write(1,"y",1);
-	printf("sth:%d", argc);
-	printf("sth:%s", argv[1]);
-	return (0);
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
