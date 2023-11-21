c46aeb788bf0e16c13c9c30e2425268410deeded
kchan <kristy.kw.chan@gmail.com>
2023-11-21 17:02:39 +1030

    work in progress (put pixel)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kchan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:46:28 by kchan             #+#    #+#             */
-/*   Updated: 2023/11/20 16:53:11 by kchan            ###   ########.fr       */
+/*   Updated: 2023/11/21 17:02:03 by kchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 // not too sure what is offset in this context
 // int offset = (y * line_length + x * (bits_per_pixel/8));
 // Note that 0x00FF0000 is the hex representation of ARGB(0,255,0,0)
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int colour)
 {
 	char	*dst;
@@ -35,6 +36,50 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 1920,1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	my_mlx_pixel_put(&img, 1000, 1000, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	my_mlx_pixel_put(&img, 6, 5, 0x00FF0000);
+	my_mlx_pixel_put(&img, 7, 5, 0x00FF0000);
+	my_mlx_pixel_put(&img, 8, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 500, 500);
 	mlx_loop(mlx);
 }
+
+// #include "../minilibx/mlx.h"
+
+
+// typedef struct	s_data {
+// 	void	*img;
+// 	char	*addr;
+// 	int		bits_per_pixel;
+// 	int		line_length;
+// 	int		endian;
+// }				t_data;
+
+
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int*)dst = color;
+// }
+
+// int	main(void)
+// {
+// 	void	*mlx;
+// 	void	*mlx_win;
+// 	t_data	img;
+
+// 	mlx = mlx_init();
+// 	mlx_win = mlx_new_window(mlx, 100, 100, "Hello world!");
+// 	img.img = mlx_new_image(mlx, 100, 100);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+// 								&img.endian);
+	
+// 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+// 	my_mlx_pixel_put(&img, 6, 5, 0x00FF0000);
+// 	my_mlx_pixel_put(&img, 7, 5, 0x00FF0000);
+// 	my_mlx_pixel_put(&img, 8, 5, 0x00FF0000);
+// 	mlx_put_image_to_window(mlx, mlx_win, img.img, 500, 500);
+// 	mlx_loop(mlx);
+// }
